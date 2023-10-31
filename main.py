41124428bcec991c475d678cc2b9b9178b37f9ed
suhaib <suhaibvbm@gmail.com>
2023-10-31 12:43:45 +0530

    Turned offed animation

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
 
-
+#screen animation offing when  0 give parameter in tracer
+screen.tracer(0)
 
 paddal = Turtle()
 paddal.shape("square")
@@ -15,6 +16,10 @@ paddal.color("white")
 paddal.shapesize(stretch_len=1,stretch_wid=5)
 paddal.penup()
 paddal.goto(x=350, y=0)
+
+
+
+
 
 #control
 
@@ -31,6 +36,10 @@ screen.listen()
 screen.onkey(go_up,"Up")
 screen.onkey(go_down,"Down")
 
+#creating while loop to turn animation on
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
