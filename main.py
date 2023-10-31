bf71a3cecc74714fb4beb185405ebdcdd7f00da7
suhaib <suhaibvbm@gmail.com>
2023-10-31 14:16:41 +0530

    completed 2 paddel left and right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Screen, Turtle
-
+from paddel import Paddle
 screen = Screen()
 
 screen.setup(width=800, height=600)
@@ -9,32 +9,18 @@ screen.title("Pong Game")
 #screen animation offing when  0 give parameter in tracer
 screen.tracer(0)
 
-paddal = Turtle()
-paddal.shape("square")
-#square shape 20 * 20 so 20*5 100
-paddal.color("white")
-paddal.shapesize(stretch_len=1,stretch_wid=5)
-paddal.penup()
-paddal.goto(x=350, y=0)
-
-
-
-
-
+r_paddel = Paddle((350, 0))
+l_paddel = Paddle((-350, 0))
 #control
-
-def go_up():
-    new_y = paddal.ycor() + 30
-    paddal.goto(paddal.xcor(),new_y)
-
-def go_down():
-    new_y = paddal.ycor() - 30
-    paddal.goto(paddal.xcor(),new_y)
 
 screen.listen()
 
-screen.onkey(go_up,"Up")
-screen.onkey(go_down,"Down")
+screen.onkey(r_paddel.go_up, "Up")
+screen.onkey(r_paddel.go_down, "Down")
+
+#
+screen.onkey(l_paddel.go_up, "w")
+screen.onkey(l_paddel.go_down, "s")
 
 #creating while loop to turn animation on
 game_is_on = True
